cedd246cd9f68d57e04005a3d6b5ee97ecdf365a
江前彪 <jiangqb@jiangqianbiaos-MacBook-Pro.local>
2022-10-24 23:05:58 +0800

    add map comp

--- pages/qweather/index.js
+++ pages/qweather/index.js
@@ -8,19 +8,53 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    longitude: null,
+    latitude: null,
+    markers: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    const self = this
     // 在小程序中发送 https 的请求
-    let data = {
-      location: '116.41,39.92'
-    }
-    api.threedays(data).then(res => {
-      console.log(res)
+    wx.getLocation({
+      type: 'wgs84',
+      success (res) {
+        const { latitude, longitude, speed, accuracy } = res
+        self.setData({
+          latitude,
+          longitude,
+          markers: [{
+            id: "0",
+            latitude,
+            longitude,
+            iconPath: "/static/images/location.png",
+            width: 40,
+            height: 40,
+            callout: {
+              'display': 'ALWAYS',
+              'fontSize': '30rpx',
+              'content': '我在这',
+              'padding': '8rpx',
+              'boxShadow': '0 0 5rpx #333',
+              'borderRadius': '4rpx'
+            }
+          }]
+        })
+        const location = `${latitude},${longitude}`
+        let data = {
+          location
+        }
+        api.threedays(data).then(res => {
+          if (res.code === 200) {
+            // 成功
+          } else {
+            // 失败
+          }
+        })
+      }
     })
   },
 
