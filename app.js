45f9c9c52570af0144303119265fe772a8ac1bd0
jiangqb <jiangqb@citycloud.com.cn>
2022-10-24 20:38:38 +0800

    first init

---
+++ app.js
@@ -0,0 +1,27 @@
+// app.js
+App({
+  onLaunch() {
+    console.log("onLaunch - 小程序加载")
+    // 展示本地存储能力
+    const logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+
+  onShow() {
+    console.log("onShow - 小程序显示")
+  },
+  onHide() {
+    console.log("onHide - 小程序显示")
+  },
+  globalData: {
+    userInfo: null
+  }
+})
---
+++ pages/test/test.js
@@ -0,0 +1,72 @@
+// pages/test/test.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    console.log("onLoad - 页面加载")
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    console.log("onReady - 页面就绪")
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+    console.log("onShow - 页面显示")
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    console.log("onHide - 页面隐藏")
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    console.log("onUnload - 页面卸载")
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  },
+
+  goIndex() {
+    wx.reLaunch({
+      url: "/pages/index/index"
+    })
+  }
+})
